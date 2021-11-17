269aa122de9ecf6c802e618d6370041ee9ac506c
Tiago Funk <tiagoff.tf@gmail.com>
2021-11-16 21:35:06 -0300

    Parâmetro errado do experimento

--- experiment.sh
+++ experiment.sh
@@ -8,7 +8,7 @@ execute_all(){
             for n in $(seq 1 $1)
             do
                 echo "$n-$file"
-                ./grasp/GRASP 1 1 1 $file --alpha 0.9 --iterations 50000 --path y --margin 1.9 --removeOperator r --removePercentage 0.15 --shuffleOperator e --addOperator b >> $2
+                ./grasp/GRASP 1 1 1 $file --alpha 0.9 --iterations 5000 --path y --margin 1.9 --removeOperator r --removePercentage 0.15 --shuffleOperator e --addOperator b >> $2
             done
         fi
     done
